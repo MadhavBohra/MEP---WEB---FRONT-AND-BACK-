d8afe87d088c0564b4b518b3fe16a8d4d9f0501c
Kal-El-pt2 <f20220052@hyderabad.bits-pilani.ac.in>
2024-06-17 15:57:32 +0530

    header addition attempted

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,103 +1,29 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import logo from './logo.png';
-import search from './search.png';
-import notifications from './Bell.png';
-import settings from './settings.png';
-import avatar from './avataricon.png';
-import './Header.css';
+import styles from '../styles/Header.module.css';
 
-interface User {
-  name: string;
-}
-
-interface HeaderProps {
-  user: User;
-}
-
-const Header: React.FC<HeaderProps> = ({ user }) => {
+const Header: React.FC = () => {
   return (
-    <header style={headerStyle}>
-      <div style={logoContainerStyle}>
-        <img src={logo} alt="Logo" style={logoStyle} />
+    <header className={styles.header}>
+      <div className={styles.logoContainer}>
+        <img src='/logo.png' alt="Logo" className={styles.logo} />
       </div>
-      <nav style={navStyle}>
-        <ul style={navListStyle}>
-          <li className="nav-item"><a href="/home" className="nav-link">Home</a></li>
-          <li className="nav-item"><a href="/aboutus" className="nav-link">About Us</a></li>
-          <li className="nav-item"><a href="/blogs" className="nav-link">Blogs</a></li>
-          <li className="nav-item"><a href="/contact" className="nav-link">Contact</a></li>
-          <li className="nav-item"><a href="/faqs" className="nav-link">FAQs</a></li>
-          <li className="nav-item"><a href="/search" className="nav-link"><img src={search} alt="Search" style={{ width: '15px', height: '15px' }} /></a></li>
+      <nav className={styles.nav}>
+        <ul className={styles.navList}>
+          <li className={styles.navItem}><a href="/home" className={styles.navLink}>Home</a></li>
+          <li className={styles.navItem}><a href="/aboutus" className={styles.navLink}>About Us</a></li>
+          <li className={styles.navItem}><a href="/blogs" className={styles.navLink}>Blogs</a></li>
+          <li className={styles.navItem}><a href="/contact" className={styles.navLink}>Contact</a></li>
+          <li className={styles.navItem}><a href="/faqs" className={styles.navLink}>FAQs</a></li>
+          <li className={styles.navItem}><a href="/search" className={styles.navLink}><img src='/search.png' alt="Search" className={styles.icon} /></a></li>
         </ul>
       </nav>
-      <div style={iconsContainerStyle}>
-        <a href="/notifications" className="icon-link"><img src={notifications} alt="Notifications" style={iconStyle1} /></a>
-        <a href="/settings" className="icon-link"><img src={settings} alt="Settings" style={iconStyle} /></a>
-        <a href="/profile" className="icon-link"><img src={avatar} alt="Avatar" style={iconStyle} /></a>
+      <div className={styles.iconsContainer}>
+        <a href="/notifications" className={styles.iconLink}><img src='/Bell.png' alt="Notifications" className={styles.icon} /></a>
+        <a href="/settings" className={styles.iconLink}><img src='/settings.png' alt="Settings" className={styles.iconLarge} /></a>
+        <a href="/profile" className={styles.iconLink}><img src='/avataricon.png' alt="Avatar" className={styles.icon} /></a>
       </div>
     </header>
   );
 };
 
-const headerStyle: React.CSSProperties = {
-  display: 'flex',
-  justifyContent: 'space-between',
-  alignItems: 'center',
-  padding: '10px 20px',
-  color: 'white',
-  width: '100vw',
-  boxSizing: 'border-box',
-  position: 'fixed',
-  top: 5,
-  left: 10,
-  zIndex: 1000,
-  fontFamily: 'Lufga, sans-serif', // Apply Lufga font to the header
-};
-
-const logoContainerStyle: React.CSSProperties = {
-  display: 'flex',
-  alignItems: 'center'
-};
-
-const logoStyle: React.CSSProperties = {
-  width: '40px',
-  height: '40px',
-  marginRight: '0px'
-};
-
-const navStyle: React.CSSProperties = {
-  flex: 1,
-  display: 'flex',
-  justifyContent: 'center'
-};
-
-const navListStyle: React.CSSProperties = {
-  listStyle: 'none',
-  display: 'flex',
-  margin: 0,
-  padding: 0
-};
-
-const iconsContainerStyle: React.CSSProperties = {
-  display: 'flex',
-  alignItems: 'center'
-};
-
-const iconStyle: React.CSSProperties = {
-  width: '35px',
-  height: '35px',
-  marginLeft: '25px'
-};
-
-const iconStyle1: React.CSSProperties = {
-  width: '55px',
-  height: '50px',
-  marginLeft: '25px'
-};
-
-const mapStateToProps = (state: RootState) => ({
-  user: state.user.user
-});
-
-export default connect(mapStateToProps)(Header);
+export default Header;
--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,7 @@ import ProfileCard from './components/ProfileCard';
 import Calendar from './components/DatePicker';
 import UpcomingEvents from './components/UpcomingEvent';
 import PostWorkoutSessionCard from './components/PostWorkout';
+import Header from './components/Header';
 
 interface Props {
   readonly children: ReactNode;
@@ -34,6 +35,7 @@ export default function RootLayout({ children }: Props) {
         <body className={styles.background}>
           <div className={styles.container}>
             <main className={styles.main}>
+              <Header />
               <div className={styles.dashstart}>
                 <header className={styles.header}>
                   <h1 className={styles.headerTitle}>Dashboard Overview</h1>
@@ -42,13 +44,22 @@ export default function RootLayout({ children }: Props) {
                   <UsernameCard
                     name="Manan Jain"
                     message="Have a nice day and don’t forget to take care of your health!"
-                  /><div className={styles.profilecard}><ProfileCard Name='Manan Jain' Age='20' Address='BITS Goa' Blood_Group='B+' Height='170cm' Weight='70kg'/>
+                  />
+                  <div className={styles.profilecard}>
+                    <ProfileCard
+                      Name="Manan Jain"
+                      Age="20"
+                      Address="BITS Goa"
+                      Blood_Group="B+"
+                      Height="170cm"
+                      Weight="70kg"
+                    />
                   </div>
-                  <div className={styles.datepicker}><Calendar/></div>         
+                  <div className={styles.datepicker}><Calendar /></div>
                   <div className={styles.graph}><BarsDataset /></div>
                   <Reminder dur1="48 min" ex1="stretching" dur2="32 min" ex2="Mind training" />
                   <div className={styles.upcomingevent}><UpcomingEvents /></div>
-                  <div className={styles.postWorkoutSessionCard}><PostWorkoutSessionCard/></div>
+                  <div className={styles.postWorkoutSessionCard}><PostWorkoutSessionCard /></div>
                   <Report weight="87" general="78" />
                   <StepCounter name="Steps Taken" steps="202" />
                   <CalorieCounter name="Calories Burned" calories="408" />
