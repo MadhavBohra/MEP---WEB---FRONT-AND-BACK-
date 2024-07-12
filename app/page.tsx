509700aabf5d2ee7d034bfee82885871fc745b5c
Sanket-0601 <f20221343@hyderabad.bits-pilani.ac.in>
2024-07-12 15:04:17 +0530

    contact Us changes

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,12 @@ import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { useRouter } from "next/navigation";
 import { app } from "./firebase";
 import UserDetailsForm from "./UserDetailEditForm/page";
-// Uncomment these imports if you want to use them
-// import UserDashboard from "./UserDashboard/page";
-// import LandingPage from "./LandingPage/page";
-// import LoginForm from "./LoginPage/page";
-// import OurServices from "./OurServices/page";
-// import Blogs from "./Blogs/page";
+//  Uncomment these imports if you want to use them
+import UserDashboard from "./UserDashboard/page";
+ import LandingPage from "./LandingPage/page";
+ import LoginForm from "./LoginPage/page";
+ import OurServices from "./OurServices/page";
+ import Blogs from "./Blogs/page";
 
 const IndexPage: React.FC = () => {
   const auth = getAuth(app);
@@ -31,7 +31,7 @@ const IndexPage: React.FC = () => {
   );
   // Uncomment the component you want to render
   // return <UserDashboard />;
-  // return <LandingPage />;
+   return <LandingPage />;
   // return <LoginForm />;
   // return <Blogs />;
   // return <OurServices />;
